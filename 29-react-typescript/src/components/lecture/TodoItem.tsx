de62db106aee04d5ec1a3ab90cb18589307fcd6b
maxbeny <maxbeny666@gmail.com>
2025-02-06 19:55:40 +0900

    up

---
+++ 29-react-typescript/src/components/lecture/TodoItem.tsx
@@ -0,0 +1,18 @@
+import { TodoItemInterface } from "../../types/interface";
+
+interface Props {
+  todo: TodoItemInterface;
+  toggleComplete: (id: number) => void;
+}
+
+export default function TodoItem({ todo, toggleComplete }: Props) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        onChange={() => toggleComplete(todo.id)}
+      />
+      <span>{todo.text}</span>
+    </li>
+  );
+}
